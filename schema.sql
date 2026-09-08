b6530d5f184d4a1e0c7d7fa318c2c48ac7698446
Tyler <spaid9876@gmail.com>
2026-09-08 14:21:06 -0500

    corrected issues with schema and seed (still incomplete). Worked on test-service tool

--- schema.sql
+++ schema.sql
@@ -2,14 +2,14 @@
 CREATE TABLE IF NOT EXISTS categories (
     category_id INT PRIMARY KEY,
     name VARCHAR(50)
-)
+);
 
 /*table of stocked ingredients*/
 CREATE TABLE IF NOT EXISTS pantry (
-    pantry_id INT UNIQUE PRIMARY KEY,
+    pantry_id INT PRIMARY KEY,
     name VARCHAR(50) UNIQUE,
-    pantry_quantity INT NOT NULL CHECK (pantry_quantity >= 0),     /*quantity as servings*/
-    FOREIGN KEY (category_id) REFERENCES categories(category_id)
+    category_id INT REFERENCES categories(category_id),
+    pantry_quantity INT NOT NULL CHECK (pantry_quantity >= 0)     /*quantity as servings*/
 );
 
 /* add database transaction*/
@@ -17,14 +17,14 @@ CREATE TABLE IF NOT EXISTS pantry (
 
 /*table of quick select recipes*/
 CREATE TABLE IF NOT EXISTS menu (
-    menu_id INT NOT NULL PRIMARY KEY UNIQUE,
+    menu_id INT PRIMARY KEY,
     name VARCHAR(50) NOT NULL UNIQUE,
-    price NOT NULL NUMERIC(5, 2),
+    price NUMERIC(5, 2) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS menu_ingredients (
     menu_id INT REFERENCES menu(menu_id),
-    pantry_id INT REFERENCES pantry(pantry_id)
+    pantry_id INT REFERENCES pantry(pantry_id),
     PRIMARY KEY (menu_id, pantry_id)
 ); 
 
@@ -44,7 +44,7 @@ CREATE TABLE IF NOT EXISTS orders (
     crust_id INT REFERENCES pantry(pantry_id),
     meat INT REFERENCES pantry(pantry_id),
     veggie INT REFERENCES pantry(pantry_id),
-    status STR,
+    status VARCHAR(50),
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
--- seed.sql
+++ seed.sql
@@ -1,12 +1,11 @@
-INSERT INTO categories (category_id, name) VALUES (
+INSERT INTO categories (category_id, name) VALUES
     (1, 'meat'),
     (2, 'vegetable'),
     (3, 'cheese'),
     (4, 'sauce'),
     (5, 'crust');
-);
 
-INSERT INTO pantry (pantry_id, name, category, pantry_quantity) VALUES (
+INSERT INTO pantry (pantry_id, name, category_id, pantry_quantity) VALUES 
     (1, 'pepperoni', 1, '5000'),
     (2, 'sausage', 1, '2500'),
     (3, 'fish', 1, '500'),
@@ -14,21 +13,18 @@ INSERT INTO pantry (pantry_id, name, category, pantry_quantity) VALUES (
     (5, 'olives', 2, '2800'),
     (6, 'cucumber', 2, '400'),
     (7, 'cheese', 3, '7500'),
-    (8, 'tomato sauce' 4, '7500'),
+    (8, 'tomato sauce', 4, '7500'),
     (9, 'alfredo sauce', 4, '850'),
     (10, 'thick crust', 5, '2000'),
     (11, 'thin crust', 5, '2000'),
     (12, 'ridiculously thin crust', 5, '35');
-);
 
-INSERT INTO menu (menu_id, name, price) VALUES (
+INSERT INTO menu (menu_id, name, price) VALUES
     (1, 'Best Pizza', 16.99),
     (2, 'Okay, That''s Fine', 14.99),
     (3, 'WHY WOULD YOU ORDER THAT??', 23.99);
-);
 
-INSERT INTO menu_ingredients (menu_id, pantry_id) VALUES (
+INSERT INTO menu_ingredients (menu_id, pantry_id) VALUES 
     (1, 1), (1, 4), (1, 7), (1, 8), (1, 10),
     (2, 2), (2, 5), (2, 7), (2, 8), (2, 11),
     (3, 3), (3, 6), (3, 7), (3, 9), (3, 12);
-);
