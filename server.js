7ab2c05c7da06635f58877fad05db7dc10dd8106
Tyler <spaid9876@gmail.com>
2026-09-09 11:31:44 -0500

    partially build chaotic-customer (working) fixed server.js (working)

--- server.js
+++ server.js
@@ -3,24 +3,18 @@ const app = express();
 const PORT = 3000;
 const { Pool } = require('pg')
 const pool = new Pool({
-    host: "locahost",
-    user: "postgres",
-    password: "make-password-safe"
-    database: "schema.sql",
-    port 5432,
+    host: 'localhost',
+    user: 'postgres',
+    password: 'make-password-safe',
+    database: 'pizzafarm',
+    port: 5432,
     max: 20
 })
 
-module.exports = pool;
-
 app.use(express.json());
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log("Server is running on port 3000!");
 });
-
-const fetchMenu => {
-    SELECT name, price FROM menu;
-}
 
 /* const fetchIngredients => {
     SELECT category_id, name FROM categories
@@ -28,10 +22,31 @@ const fetchMenu => {
 } */ /* finish building custom pizza generation */
 
 app.get('/api/menu', async (req, res) => {
+    const string = 'SELECT menu_id FROM menu;'
     try {
-        dbResult = await pool.query({fetchMenu})
-        return res.json({dbResult});
+        const response = await pool.query(string)
+        if (response.rows.length > 0) {
+            return res.json({success: true, data: response.rows});
+        } else {
+            return res.status(404).json({ success: false, message: "No records found" })
+        }
     } catch (error) {
+        console.error(error);
+        res.json({success: false, error: "There was an error in app.get /api/menu"})
+    }
+});
+
+app.get('/api/ingredients', async (req, res) => {
+    const string = 'SELECT pantry_id, category_id FROM ingredients;'
+    try {
+        const response = await pool.query(string)
+        if (response.rows.length > 0) {
+            return res.json({success: true, data: response.rows});
+        } else {
+            return res.status(404).json({ success: false, message: "No records found" })
+        }
+    } catch (error) {
+        console.error(error);
         res.json({success: false, error: "There was an error in app.get /api/menu"})
     }
 });
